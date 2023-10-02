de0dc90a3be83571bc0e431b43f3a7fbdf2015e5
Zachary Scott <adult.zachary@gmail.com>
2023-10-01 20:15:54 -0500

    LED Change

--- LED.py
+++ LED.py
@@ -2,13 +2,13 @@ from machine import Pin, PWM
 import time
 
 pwm = PWM(Pin(15))
-pwm.freq(1000)
+pwm.freq(8)
 
 try:
-    for i in range(0,10000):
+    for i in range(0,65535):
         pwm.duty_u16(i)
         time.sleep_us(100)
-    for i in range(20000,0,-1):
+    for i in range(65535,0,-1):
         pwm.duty_u16(i)
         time.sleep_us(100)
 except:
